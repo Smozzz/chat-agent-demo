bfc9f8936f68d46a8e2efd2bb497e8b54edd34da
Smozzz <1258083176@qq.com>
2026-07-14 16:43:50 +0800

    successfully create an api key

---
+++ chat.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client=OpenAI(
+    base_url=os.getenv("OPENAI_BASE_URL"),
+    api_key=os.getenv('OPENAI_API_KEY'),
+)
+
+response = client.responses.create(
+    model="gpt-5.4-mini",
+    input="你好"
+)
+
+print(response.output_text)
