4770554df56ffa8aaf0e8dfbd001d32e852258ae
Smozzz <1258083176@qq.com>
2026-07-14 16:56:23 +0800

    简单的多轮聊天memory“

--- chat.py
+++ chat.py
@@ -9,9 +9,40 @@ client=OpenAI(
     api_key=os.getenv('OPENAI_API_KEY'),
 )
 
-response = client.responses.create(
-    model="gpt-5.4-mini",
-    input="你好"
-)
+# 聊天历史
+messages=[]
 
-print(response.output_text)
+while True:
+    user_input = input("User: ")
+
+    # 退出
+    if user_input == "exit":
+        break
+
+    # 添加用户消息
+    messages.append(
+        {
+            "role": "user",
+            "content": user_input
+        }
+    )
+
+    response = client.responses.create(
+        model="gpt-5.4-mini",
+        input=messages
+    )
+
+    answer = response.output_text
+
+    print(
+        "Assistant:",
+        answer
+    )
+
+    # 保存模型回答
+    messages.append(
+        {
+            "role": "assistant",
+            "content": answer
+        }
+    )
